b269548df8ff20ee1384194ac8914ad6873bbd1f
Ni Runyu <ni-runyu@ed.tmu.ac.jp>
2023-07-15 14:01:24 +0900

    修改了case study的代码,与RSETC相匹配.

--- code/tryVisualization.py
+++ code/tryVisualization.py
@@ -2,8 +2,8 @@
 Author: Ni Runyu ni-runyu@ed.tmu.ac.jp
 Date: 2023-02-10 13:37:11
 LastEditors: Ni Runyu ni-runyu@ed.tmu.ac.jp
-LastEditTime: 2023-02-17 14:56:15
-FilePath: /undefined/home/ni/Desktop/try/code/tryVisualization.py
+LastEditTime: 2023-07-15 13:59:56
+FilePath: /ESETC/code/tryVisualization.py
 Description: 输出对于特定实体的类型权重，以及对于特定关系的类型权重
 
 Copyright (c) 2023 by Ni Runyu ni-runyu@ed.tmu.ac.jp, All Rights Reserved. 
@@ -27,7 +27,7 @@ from pykeen.datasets import YAGO310, FB15k237
 from pykeen.datasets import analysis as data_analysis
 from pykeen.evaluation import RankBasedEvaluator
 from pykeen.triples import TriplesFactory
-from utilities import get_white_list_relation, readTypeData
+from utilities import load_dataset
 
 HEAD = 0
 TAIL = 2
@@ -56,7 +56,7 @@ def draw_example(data, save_path, format='png'):
     save_path = save_path + '.' + format
     plt.savefig(save_path, format=format)
 
-def find_k_relevant_type(data, id, string2id, type2id, mode = 'rel', flag = 'before', de = None, k = 1):
+def find_k_relevant_type(data, id, string2id, type2id, path, mode = 'rel', flag = 'before', de = None, k = 1):
     # 用来查看某个特例的类型权重
     types = np.array(list(type2id.keys()))
     string = list(string2id.keys())
@@ -65,7 +65,7 @@ def find_k_relevant_type(data, id, string2id, type2id, mode = 'rel', flag = 'bef
     if id >= len(string):
         rev = '-reverse'
         id = id - len(string)
-    figure_floder = os.path.join('../models/', dataset, 'figures/')
+    figure_floder = os.path.join(path, 'figures/')
     if not os.path.exists(figure_floder):
         os.makedirs(figure_floder)
     if mode == 'rel':
@@ -79,8 +79,8 @@ def find_k_relevant_type(data, id, string2id, type2id, mode = 'rel', flag = 'bef
         print('for relation %s, the most relevant type is: '%(string[id]+rev), types[np.argsort(-data[0, id, :])[:k]], 'with weight: ', data[0, id, :][np.argsort(-data[0, id, :])[:k]])
         print('for relation %s, the most irrelevant type is: '%(string[id]+rev), types[np.argsort(data[0, id, :])[:k]], 'with weight: ', data[0, id, :][np.argsort(data[0, id, :])[:k]])
         print('for tail')
-        print('for relation %s, the most relevant type is: '%(string[id]+rev), types[np.argsort(-data[0, id, :])[:k]], 'with weight: ', data[1, id, :][np.argsort(-data[1, id, :])[:k]])
-        print('for relation %s, the most irrelevant type is: '%(string[id]+rev), types[np.argsort(data[0, id, :])[:k]], 'with weight: ', data[1, id, :][np.argsort(data[1, id, :])[:k]])
+        print('for relation %s, the most relevant type is: '%(string[id]+rev), types[np.argsort(-data[1, id, :])[:k]], 'with weight: ', data[1, id, :][np.argsort(-data[1, id, :])[:k]])
+        print('for relation %s, the most irrelevant type is: '%(string[id]+rev), types[np.argsort(data[1, id, :])[:k]], 'with weight: ', data[1, id, :][np.argsort(data[1, id, :])[:k]])
         draw_example(data[0, id, :], example_path_h, format='svg')
         draw_example(data[1, id, :], example_path_t, format='svg')
     else:
@@ -93,102 +93,75 @@ def find_k_relevant_type(data, id, string2id, type2id, mode = 'rel', flag = 'bef
         print('for entity %s, the most irrelevant type is: '%(string[id]), types[np.argsort(data[id, :])[:k]], 'with weight: ', data[id, :][np.argsort(data[id, :])[:k]])
         draw_example(data[id, :], example_path, format='svg')
 
-        
 
 
-def splitTypeData(data:TriplesFactory, type_position = 0) -> "LabeledTriples, LabeledTriples":
-    unlike_type_rel, like_type_rel = get_white_list_relation(data, type_position=type_position)
+if __name__ == '__main__':
 
-    return data.label_triples(data.new_with_restriction(relations=unlike_type_rel).mapped_triples), data.label_triples(data.new_with_restriction(relations=like_type_rel).mapped_triples), unlike_type_rel, like_type_rel
-
-
-
-def load_data(IfUseTypeLike, d):
-    
-    relevant_ent = list()
-    if IfUseTypeLike:
-        training_triples, training_type_triples, unlike_type_rel, like_type_rel = splitTypeData(d.training, type_position=TAIL)
-        training_data = TriplesTypesFactory.from_labeled_triples(triples=training_triples, type_triples=training_type_triples, type_position=TAIL, create_inverse_triples=True)
-        for triple in training_type_triples:
-            try:
-                training_data.entity_to_id[triple[HEAD]]
-                relevant_ent.append(triple[HEAD])
-            except:
-                # print(triple[HEAD])
-                pass
-        validation = TriplesFactory.from_labeled_triples(
-                    d.validation.label_triples(d.validation.mapped_triples), 
-                    entity_to_id=training_data.entity_to_id, 
-                    relation_to_id=training_data.relation_to_id,
-                    create_inverse_triples=True)
-        validation = validation.new_with_restriction(relations=unlike_type_rel)
-        testing = TriplesFactory.from_labeled_triples(
-                        d.testing.label_triples(d.testing.mapped_triples), 
-                        entity_to_id=training_data.entity_to_id, 
-                        relation_to_id=training_data.relation_to_id,
-                        create_inverse_triples=True)
-        testing = testing.new_with_restriction(relations=unlike_type_rel)
-        testing = testing.new_with_restriction(entities=relevant_ent)
-
-    else:
-        training_data = d.training
-        validation = d.validation
-        testing = d.testing.new_with_restriction(entities=relevant_ent)
-
-    return training_data, validation, testing
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--dataset', choices=['FB15k237', 'YAGO3-10', 'fb15k-237-type', 'CAKE-FB15K237', 'CAKE-FB15K', 'CAKE-NELL-995', 'CAKE-DBpedia-242'], default='CAKE-FB15K237')
-parser.add_argument('-reverse', '--CreateInverseTriples', action='store_true', default=False)
-parser.add_argument('-t', '--IfUseTypeLike', action='store_true', default=False)
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset', choices=['FB15k237', 'YAGO3-10', 'fb15k-237-type', 'CAKE-FB15K237', 'CAKE-FB15K', 'CAKE-NELL-995', 'CAKE-DBpedia-242', 'yago5k-106'], default='yago5k-106')
+    parser.add_argument('-reverse', '--CreateInverseTriples', action='store_true', default=False)
+    parser.add_argument('-t', '--IfUseTypeLike', action='store_true', default=False)
+    args = parser.parse_args()
 
 
-# de = 'PreTrainTypeEmb'
-de = 'noDescription'
-# date = '20230225-001357'
-m_name = 'ESETCwithTransE'
-# m_name = 'CatESETCwithTransE'
-date = '20230225-064727'
+    # de = 'PreTrainTypeEmb'
+    de = 'noDescriptionActivationFuncionWeightMask'
+    # date = '20230225-001357'
+    m_name = 'CatRSETCwithRotate'
+    # m_name = 'CatESETCwithTransE'
+    date = '20230713-121052'
 
-dataset = args.dataset
-model_path = os.path.join('../models/', dataset, de, m_name, date)
-model = torch.load(model_path+'/trained_model.pkl')
+    dataset = args.dataset
+    model_path = os.path.join('../models/', dataset, de, m_name, date)
+    model = torch.load(model_path+'/trained_model.pkl')
 
-if args.dataset == 'fb15k-237-type':
-    training_data, validation, testing = readTypeData(args.dataset, data_pro_func=splitTypeData, type_position=HEAD, create_inverse_triples=args.CreateInverseTriples)
-elif 'CAKE' in args.dataset:
-    training_data, validation, testing = readTypeData(args.dataset, data_pro_func=splitTypeData, type_position=TAIL, create_inverse_triples=args.CreateInverseTriples)
-else:
-    if args.dataset == 'FB15k237':
-            data = FB15k237(create_inverse_triples = args.CreateInverseTriples)
-    elif args.dataset == 'YAGO3-10':
-        data = YAGO310(create_inverse_triples = args.CreateInverseTriples)
+    training_data, validation, testing = load_dataset(dataset)
+
+    if args.IfUseTypeLike:
+        dataset = dataset + '-TypeLike'
 
 
-    training_data, validation, testing = load_data(args.IfUseTypeLike, data)
+    vis_path = os.path.join('../result/visualization/', dataset, de, m_name, date)
+    if not os.path.exists(vis_path):
+        os.makedirs(vis_path) 
 
-if args.IfUseTypeLike:
-    dataset = dataset + '-TypeLike'
-
-heatmap_before_save_path = os.path.join('../models/', dataset, 'ent_type_weight_beforeTraining')
-heatmap_before_save_path_rel_h = os.path.join('../models/', dataset, 'rel_type_weight_beforeTraining_h')
-heatmap_before_save_path_rel_t = os.path.join('../models/', dataset, 'rel_type_weight_beforeTraining_t')
-
-
-# draw_heatmap(training_data.ents_types, 'entity type weight before training', heatmap_before_save_path)
+    # heatmap_before_save_path = os.path.join(vis_path, 'ent_type_weight')
+    heatmap_before_save_path_rel_h = os.path.join(vis_path,'rel_type_weight_beforeTraining_h')
+    heatmap_before_save_path_rel_t = os.path.join(vis_path, 'rel_type_weight_beforeTraining_t')
 
 
-# example_id = np.where(np.sum(training_data.ents_types, axis=1)== 0)[0][3]
-ent_list = ['/m/01z4y', '/m/02cllz', '/m/03p41', '/m/0dnqr', '/m/09th87']
-for ent in ent_list: 
-    example_id = training_data.entity_to_id[ent]
+    draw_heatmap(training_data.rels_types[0], 'relation head type weight before training', heatmap_before_save_path_rel_h)
+    draw_heatmap(training_data.rels_types[1], 'relation tail type weight before training', heatmap_before_save_path_rel_t)
 
-    # find_k_relevant_type(training_data.ents_types, example_id, training_data.entity_to_id, training_data.types_to_id, mode='entity', de=de, k=5)
-    # num_relations = len(training_data.relation_to_id)
+    # heatmap_after_save_path = os.path.join('../models/', dataset, 'ent_type_weight_afterTraining')
+    heatmap_after_save_path_rel_h = os.path.join(vis_path, 'rel_type_weight_afterTraining_h')
+    heatmap_after_save_path_rel_t = os.path.join(vis_path, 'rel_type_weight_afterTraining_t')
+
+    rel_types_after_training = model.rels_types.data.cpu().numpy()
+
+    draw_heatmap(rel_types_after_training[0], 'relation head type weight after training', heatmap_after_save_path_rel_h)
+    draw_heatmap(rel_types_after_training[1], 'relation tail type weight after training', heatmap_after_save_path_rel_t)
+
+    rel_label = ['worksAt', 'hasGender']
+    for rel in rel_label:
+        example_id = training_data.relation_to_id[rel]
+        print('before training')
+        find_k_relevant_type(training_data.rels_types, example_id, training_data.relation_to_id, training_data.types_to_id, path=vis_path, mode='rel', de=de, k=5, flag='before')
+        print('after training')
+        find_k_relevant_type(rel_types_after_training, example_id, training_data.relation_to_id, training_data.types_to_id, path=vis_path, mode='rel', de=de, k=5, flag='after')
 
 
-    ents_types = model.ents_types.data.cpu().numpy()
-    # draw_heatmap(ents_types, 'entity type weight after training', os.path.join(model_path, 'ent_type_weight_afterTraining'))
 
-    find_k_relevant_type(ents_types, example_id, training_data.entity_to_id, training_data.types_to_id, mode='ent', flag='after', de=de, k=5)
+    # example_id = np.where(np.sum(training_data.ents_types, axis=1)== 0)[0][3]
+    # ent_list = ['/m/01z4y', '/m/02cllz', '/m/03p41', '/m/0dnqr', '/m/09th87']
+    # for ent in ent_list: 
+    #     example_id = training_data.entity_to_id[ent]
+
+        # find_k_relevant_type(training_data.ents_types, example_id, training_data.entity_to_id, training_data.types_to_id, mode='entity', de=de, k=5)
+        # num_relations = len(training_data.relation_to_id)
+
+
+        # ents_types = model.ents_types.data.cpu().numpy()
+        # draw_heatmap(ents_types, 'entity type weight after training', os.path.join(model_path, 'ent_type_weight_afterTraining'))
+
+        # find_k_relevant_type(ents_types, example_id, training_data.entity_to_id, training_data.types_to_id, mode='ent', flag='after', de=de, k=5)
